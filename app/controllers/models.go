9aa1be6e03ec1bc16492b084de079824648caa85
Aly Kafoury <aly.kafoury@gmail.com>
2018-08-15 05:05:07 +0200

    Captilize letters to extract

--- app/controllers/models.go
+++ app/controllers/models.go
@@ -9,13 +9,12 @@ import (
 
 type model map[string]string
 type models map[string]model
-type updateRequest map[string]interface{}
-// type updateRequest struct {
-// 	dropColumn   []string `json:dropColumn`
-// 	createColumn map[string]string `json:createColumn`
-// 	renameColumn map[string]string `json:renameColumn`
-//   updateColumn map[string]string `json:updateColumn`
-// }
+type updateRequest struct {
+	DropColumn   []string `json:DropColumn`
+	CreateColumn map[string]string `json:CreateColumn`
+	RenameColumn map[string]string `json:RenameColumn`
+  UpdateColumn map[string]string `json:UpdateColumn`
+}
 
 func ListModels(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("List..!"))
@@ -46,24 +45,14 @@ func SetModels(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateModel(w http.ResponseWriter, r *http.Request) {
-  // test_model := updateRequest{dropColumn: []string{"g", "h", "i"},
-  //                              createColumn:  map[string]string{"fix":"bex"},
-  //                              renameColumn:  map[string]string{"fix":"bex"},
-  //                              updateColumn:  map[string]string{"fix":"bex"}}
   var update_model updateRequest
   vars := mux.Vars(r)
   decoder := json.NewDecoder(r.Body)
   decoder.Decode(&update_model)
-  // out, err := json.Marshal(test_model)
-  // if err != nil {
-  //     fmt.Println(err)
-  //     return
-  // }
-  // fmt.Println("%v+", out)
+
   fmt.Println("%v+", update_model)
   fmt.Println("%v+", vars)
-  // json.NewEncoder(w).Encode(test_model)
-  //w.Write([]byte(update_model))
+  json.NewEncoder(w).Encode(update_model)
 }
 
 func DeleteModel(w http.ResponseWriter, r *http.Request) {
